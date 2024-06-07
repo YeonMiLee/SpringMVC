974b44d82e4aba145259cd00192ed1715c7c26ff
YeonMiLee <160806889+YeonMiLee@users.noreply.github.com>
2024-06-07 09:41:00 +0900

    Add files via upload

---
+++ ch02_pjt_03/src/ch02_pjt_03/MainClass.java
@@ -0,0 +1,18 @@
+package ch02_pjt_03;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		new CalAssembler();		//어셈블러 객체만 생성
+		
+		/*
+		 * MyCalculator calculator = new MyCalculator();
+		 * 
+		 * calculator.calculate(10, 5, new CalAdd()); calculator.calculate(10, 5, new
+		 * CalSub()); calculator.calculate(10, 5, new CalMul());
+		 * calculator.calculate(10, 5, new CalDiv());
+		 */
+	}
+
+}
